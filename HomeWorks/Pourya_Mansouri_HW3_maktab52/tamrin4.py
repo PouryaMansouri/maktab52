5c5068fe9616a718b0f18f29c14d8d33cd05d6f6
Pourya <pourya@pop-os.localdomain>
2021-04-08 18:35:11 +0430

    HW3 tamrin4

---
+++ HomeWorks/Pourya_Mansouri_HW3_maktab52/tamrin4.py
@@ -0,0 +1,54 @@
+class Account:
+    name: str
+    balance_account: int
+    minimum_balance: int
+    __minimum_balance = 100
+
+    def __init__(self, name: str, balance_account: int):
+        self.__check_balance(balance_account)
+        self.__balance_account = balance_account
+        self.name = name
+
+    def deposit(self, amount: int):
+        self.__balance_account += amount
+        return f"{self.name}: deposit complete and {amount}$ add to your account " \
+               f"and new account balance is {self.__balance_account}"
+
+    def withdraw(self, amount):
+        self.__check_balance(self.__balance_account - amount)
+        self.__balance_account -= amount
+        return f"{self.name}: withdraw complete and {amount}$ decrease from your account " \
+               f"and new account balance is {self.__balance_account}"
+
+    def __repr__(self):
+        return f"{self.name} has {self.__balance_account}$ in account"
+
+    # @staticmethod
+    # def __check_balance(amount):
+    #     if amount >= __minimum_balance:
+    #         raise Exception("You hove not enough account balance to withdraw this amount")
+
+    def __check_balance(self, amount):
+        if amount <= self.__class__.__minimum_balance:
+            raise Exception("You hove not enough account balance")
+
+    # TODO: how add return instead of print in withdraw and deposit
+    def transaction(self, account, amount):
+        r = ''
+        r += self.withdraw(amount) + '\n'
+        r += account.deposit(amount)
+        return r
+
+
+pourya = Account("Pourya", 200)
+ali = Account("Ali", 700)
+
+print(pourya)
+print(pourya.deposit(300))
+print(pourya)
+print(pourya.withdraw(100))
+print(pourya)
+print()
+print(ali)
+print()
+print(ali.transaction(pourya, 400))
