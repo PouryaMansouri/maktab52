9ac5b3958c560d53dd9ea5c1b18abb1534d69010
Pourya Mansouri <pourya.mansouri@hotmail.com>
2021-04-20 09:40:40 +0430

    add property and setter to date

--- HomeWorks/Pourya_Mansouri_HW3_maktab52/tamrin2_resolve.py
+++ HomeWorks/Pourya_Mansouri_HW3_maktab52/tamrin2_resolve.py
@@ -13,6 +13,15 @@ class Birthday:
 
     def __init__(self, date: tuple):
         self.__birthday_tuple = date
+        self.birthday_date = date
+
+    @property
+    def birthday_date(self):
+        return self.__birthday_date
+
+    @birthday_date.setter
+    def birthday_date(self, date):
+        assert isinstance(date, tuple) and 1910 <= date[0] < 2022 and 1 <= date[1] <= 12 and 1 <= date[2] < 31
         self.__birthday_date = datetime(*date)
 
     @staticmethod
@@ -22,11 +31,11 @@ class Birthday:
 
     @property
     def years(self) -> int:
-        return int(Birthday.calculate_delta_time(self.__birthday_date).days // 365)
+        return int(Birthday.calculate_delta_time(self.birthday_date).days // 365)
 
     @property
     def hours(self) -> int:
-        return int(Birthday.calculate_delta_time(self.__birthday_date).total_seconds() // 3600)
+        return int(Birthday.calculate_delta_time(self.birthday_date).total_seconds() // 3600)
 
     def days_to_birthday(self):
         __birthday = self.__birthday_tuple[1:]
@@ -38,7 +47,8 @@ class Birthday:
         return f"{abs(Birthday.calculate_delta_time(__next_birthday).days)} days to your next birthday"
 
     def __repr__(self) -> str:
-        return f'you are {self.years} years old or {self.hours} hours old'
+        return f'you are {self.years} years old or {self.hours} hours old\n' \
+               f"your birthday is {self.birthday_date}"
 
 
 my_birthday_date = 1991, 3, 21, 11
@@ -46,3 +56,5 @@ my_birthday_date = 1991, 3, 21, 11
 b1 = Birthday(my_birthday_date)
 print(b1)
 print(b1.days_to_birthday())
+print()
+b1.birthday_date = 1991, 22, 21, 11
