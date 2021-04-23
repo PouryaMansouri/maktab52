2507a17f0d60ab734cfbdef21f8cf675aea167c8
Pourya Mansouri <pourya.mansouri@hotmail.com>
2021-04-23 20:12:10 +0430

    modify HW5 tarmin 3

--- HomeWorks/Pourya_Mansouri_HW5_maktab52/tamrin3.py
+++ HomeWorks/Pourya_Mansouri_HW5_maktab52/tamrin3.py
@@ -6,13 +6,17 @@ def file_size_in_folder(file_path):
     items_list = os.listdir(file_path)
     files_size = 0
     file_extension = re.compile('\..*')
+    file_list = []
     for _ in items_list:
         if os.path.isfile(_):
-            if len(file_extension.findall(_)[0][1:]) >= 3:
+            if len(file_extension.findall(_)[0][1:]) >= 5:
                 files_size += os.path.getsize(_)
-
-    return files_size
+                file_list.append(_)
+    return files_size, file_list
 
 
 path = "./"
-print(file_size_in_folder(path))
+
+files_size, file_list = file_size_in_folder(path)
+print("file list:\n", file_list)
+print("\nfile size: \n", files_size)
