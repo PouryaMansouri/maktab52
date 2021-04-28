a0b1523c39de3c4559a4f41d35ba651ef3b8095f
Pourya <pourya.mansouri@hotmail.com>
2021-04-28 22:28:11 +0430

    some change

--- HomeWorks/Pourya_Mansouri_HW6_maktab52/tamrin2.py
+++ HomeWorks/Pourya_Mansouri_HW6_maktab52/tamrin2.py
@@ -1,17 +1,35 @@
-# class MatException(Exception):
+class MatException(Exception):
+    def __init__(self, msg: str, data: str):
+        self.msg = msg
+        self.data = data
+        super().__init__(f"invalid input to calculate, your input: '{data}'")
 
 
-def calculation(input: str) -> None:
-    try:
-        out = eval(input)
-    except:
-        print("invalid input to calculate")
-    else:
-        print(out)
+class Calculation:
+
+    def __init__(self, phrase: str):
+        self.phrase = phrase
+
+    @property
+    def phrase(self):
+        return self.__phrase
+
+    @phrase.setter
+    def phrase(self, phrase):
+
+        self.__phrase = phrase
+
+    def calculation(input: str) -> None:
+        try:
+            out = eval(input)
+        except:
+            print("invalid input to calculate")
+        else:
+            print(out)
 
 
 in1 = '3+4'
 in2 = '6*4+'
 
-calculation(in1)
-calculation(in2)
+c = Calculation(in1)
+c.ph = in2
