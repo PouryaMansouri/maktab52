b19f213f4f96fec008e367ac9c1238dc0f5aaec9
pourya <pourya.mansouri@hotmail.com>
2021-07-08 16:46:05 +0430

    session tamam

--- s042_14000417_django1/profile/profile_dj/settings.py
+++ s042_14000417_django1/profile/profile_dj/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'profile_dj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # TODO: nice
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
